a8d3048252c9a37c442af87d3b483d38b0290070
Preisler <preisler.andras@gmail.com>
2023-07-19 21:23:31 +0200

    init front end

--- src/components/core/navbar/Navbar.js
+++ src/components/core/navbar/Navbar.js
@@ -2,10 +2,10 @@ const Navbar = () => {
     return (
         <nav className="navbar">
             <div className="navbar-container">
-                <h1>KoczkaBased v2.0</h1>
+                <h1 className="navbar-title">Koczka Based  v2.0</h1>
             </div>
         </nav>
-    );
+    ); 
 }
 
 export default Navbar;
---
+++ src/components/post/Post.js
@@ -0,0 +1,9 @@
+const Post = ({ post }) => {
+    const { title, body } = post;
+    return (
+        <div className="post-container">
+            <h2>{title}</h2>
+            <div>{body}</div>
+        </div>
+    );
+}
